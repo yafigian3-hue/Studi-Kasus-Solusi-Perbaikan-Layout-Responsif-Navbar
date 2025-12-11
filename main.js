48055b29880a8e9c7a32b88efdba2e0ef83347b1
juned <yafigian3@gmail.com>
2025-12-11 09:35:00 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+  // Toggle menu mobile
+  const menuBtn = document.getElementById("menuBtn");
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
